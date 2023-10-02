105a8bd03e5d4b66b20e931acfc7ce90d277c621
Denys Vedernykov <razom.info@gmail.com>
2023-10-02 21:36:15 +0300

    Initial commit

---
+++ MultithreadedQueueProcessingService.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Concurrent;
+
+namespace Multi_threaded_Processing
+{
+    public class MultithreadedQueueProcessingService<T>
+    {
+        private readonly List<Thread> _threads = new List<Thread>();
+        private readonly ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+
+        private int _countEmptyQueue = 0;
+
+        public MultithreadedQueueProcessingService()
+        {
+            InitThreads();
+        }
+
+        #region -- IMultithreadedQueueProcessingService implementation --
+
+        public int ThreadCount { get; set; } = 3;
+
+        public delegate void WorkerType(T value);
+
+        public WorkerType Worker { get; set; }
+
+        public void AddToQueue(T value)
+        {
+            _queue.Enqueue(value);
+        }
+
+        public void RunThreads()
+        {
+            foreach (var thread in _threads)
+            {
+                thread.Start();
+            }
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private void InitThreads()
+        {
+            _threads.Clear();
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                _threads.Add(new Thread(ThreadWorker));
+            }
+        }
+
+        private void ThreadWorker()
+        {
+            T value;
+
+            while (true)
+            {
+                if (_queue.TryDequeue(out value))
+                {
+                    _countEmptyQueue = 0;
+
+                    Worker(value);
+                }
+                else
+                {
+                    _countEmptyQueue++;
+
+                    var delay = _countEmptyQueue >= 20000 ? 600 : 100 + (int)(500 * _countEmptyQueue / 20000f);
+
+                    Task.Delay(delay).Wait();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Multi_threaded_Processing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new MultithreadedQueueProcessingService<int>();
+
+            service.Worker = (value) => Console.WriteLine(value);
+
+            service.RunThreads();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                service.AddToQueue(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
